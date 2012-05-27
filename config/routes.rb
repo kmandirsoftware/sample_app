127048c7321f7f872e0c54cc03e2ed843af38dc5
kmandirsoftware <kgerhads@comcast.net>
2012-05-27 11:37:17 -0600

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  get "users/new"
 
-  get "static_pages/about"
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
